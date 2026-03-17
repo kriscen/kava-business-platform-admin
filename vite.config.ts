c8dcadfd638dea028a2a3c29b010928e3c6fee74
Kris <kriscen@163.com>
2026-03-17 10:58:58 +0800

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
 import path from 'path'
 
@@ -10,6 +11,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
+      tailwindcss(),
       viteMockServe({
         mockPath: 'mock',
         enable: env.VITE_ENABLE_MOCK === 'true',
